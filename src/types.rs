5ef8187d0c7ef43804f3c0ca4aca35a17cd7ecde
qxbthree <qxbthree@gmail.com>
2025-01-16 02:17:14 +0800

    fix bugs

--- src/types.rs
+++ src/types.rs
@@ -2,12 +2,12 @@
 pub struct PinnedRepo {
     pub name: String,
     pub description: String,
-    pub stars: u32,
-    pub forks: u32
+    pub stars: Option<i32>,
+    pub forks: Option<i32>
 }
 
 impl PinnedRepo {
-    pub fn new(name: String, description: String, stars: u32, forks: u32) -> Self {
+    pub fn new(name: String, description: String, stars: Option<i32>, forks: Option<i32>) -> Self {
         Self {
             name,
             description,
--- src/utils.rs
+++ src/utils.rs
@@ -26,10 +26,29 @@ pub fn get_pinned_repos(html: &str) -> PinnedResult {
             .get_elements_by_class_name("pinned-item-meta")
             .collect::<Vec<NodeHandle>>();
 
+        // name & description should be present at all times
         let name = name_dom.first().unwrap().get(pinned_parser).unwrap().inner_text(pinned_parser).trim().to_string();
         let description = description_dom.first().unwrap().get(pinned_parser).unwrap().inner_text(pinned_parser).trim().to_string();
-        let stars = pinned_meta.first().unwrap().get(pinned_parser).unwrap().inner_text(pinned_parser).trim().parse::<u32>().unwrap();
-        let forks = pinned_meta.last().unwrap().get(pinned_parser).unwrap().inner_text(pinned_parser).trim().parse::<u32>().unwrap();
+
+        let stars = match pinned_meta.first() {
+            Some(description) => Some(description.get(pinned_parser)
+                                            .ok_or("Failed to parse stars dom".to_string())?
+                                            .inner_text(pinned_parser)
+                                            .trim()
+                                            .parse::<i32>()
+                                            .map_err(|err| format!("Failed to convert stars element to i32: {err}"))?),
+            None => None
+        };
+
+        let forks = match pinned_meta.last() {
+            Some(description) => Some(description.get(pinned_parser)
+                                            .ok_or("Failed to parse forks dom".to_string())?
+                                            .inner_text(pinned_parser)
+                                            .trim()
+                                            .parse::<i32>()
+                                            .map_err(|err| format!("Failed to convert forks element to i32: {err}"))?),
+            None => None
+        };
 
         repos.push(PinnedRepo::new(
             name,
