d891234350bbb51051e133c560ed5693d8bf1a4c
qxbthree <qxbthree@gmail.com>
2025-01-16 02:44:17 +0800

    added language on PinnedRepo

--- src/types.rs
+++ src/types.rs
@@ -3,16 +3,39 @@ pub struct PinnedRepo {
     pub name: String,
     pub description: String,
     pub stars: Option<i32>,
-    pub forks: Option<i32>
+    pub forks: Option<i32>,
+    pub language: Option<Language>
 }
 
 impl PinnedRepo {
-    pub fn new(name: String, description: String, stars: Option<i32>, forks: Option<i32>) -> Self {
+    pub fn new(
+        name: String,
+        description: String,
+        stars: Option<i32>,
+        forks: Option<i32>,
+        language: Option<Language>
+    ) -> Self {
         Self {
             name,
             description,
             stars,
-            forks
+            forks,
+            language
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Language {
+    pub color: String,
+    pub name: String
+}
+
+impl Language {
+    pub fn new(color: String, name: String) -> Self {
+        Self {
+            color,
+            name
         }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use tl::NodeHandle;
 
-use crate::{PinnedRepo, PinnedResult};
+use crate::{Language, PinnedRepo, PinnedResult};
 
 pub fn get_pinned_repos(html: &str) -> PinnedResult {
     let mut repos: Vec<PinnedRepo> = vec![];
@@ -26,12 +26,19 @@ pub fn get_pinned_repos(html: &str) -> PinnedResult {
             .get_elements_by_class_name("pinned-item-meta")
             .collect::<Vec<NodeHandle>>();
 
+        let language_color_dom = pinned_dom
+            .get_elements_by_class_name("repo-language-color")
+            .collect::<Vec<NodeHandle>>();
+
+        let language_name_dom = pinned_dom
+            .query_selector("span[itemprop=\"programmingLanguage\"]");
+
         // name & description should be present at all times
         let name = name_dom.first().unwrap().get(pinned_parser).unwrap().inner_text(pinned_parser).trim().to_string();
         let description = description_dom.first().unwrap().get(pinned_parser).unwrap().inner_text(pinned_parser).trim().to_string();
 
         let stars = match pinned_meta.first() {
-            Some(description) => Some(description.get(pinned_parser)
+            Some(stars) => Some(stars.get(pinned_parser)
                                             .ok_or("Failed to parse stars dom".to_string())?
                                             .inner_text(pinned_parser)
                                             .trim()
@@ -41,20 +48,52 @@ pub fn get_pinned_repos(html: &str) -> PinnedResult {
         };
 
         let forks = match pinned_meta.last() {
-            Some(description) => Some(description.get(pinned_parser)
-                                            .ok_or("Failed to parse forks dom".to_string())?
-                                            .inner_text(pinned_parser)
-                                            .trim()
-                                            .parse::<i32>()
-                                            .map_err(|err| format!("Failed to convert forks element to i32: {err}"))?),
+            Some(forks) => Some(forks.get(pinned_parser)
+                                        .ok_or("Failed to parse forks dom".to_string())?
+                                        .inner_text(pinned_parser)
+                                        .trim()
+                                        .parse::<i32>()
+                                        .map_err(|err| format!("Failed to convert forks element to i32: {err}"))?),
             None => None
+        };
+
+        let language_color = match language_color_dom.first() {
+            Some(language_color) => Some(language_color.get(pinned_parser)
+                                                            .ok_or("Failed to parse language color dom".to_string())?
+                                                            .as_tag()
+                                                            .unwrap()
+                                                            .attributes()
+                                                            .get("style")
+                                                            .unwrap()
+                                                            .unwrap()
+                                                            .as_utf8_str()
+                                                            .replace("background-color: ", "")),
+            None => None
+        };
+
+        let language_name = match language_name_dom {
+            Some(language_name) => Some(language_name.collect::<Vec<NodeHandle>>()
+                                                        .first()
+                                                        .unwrap()
+                                                        .get(pinned_parser)
+                                                        .ok_or("Failed to parse language name dom".to_string())?
+                                                        .inner_text(pinned_parser)
+                                                        .trim()
+                                                        .to_string()),
+            None => None
+        };
+
+        let language = match (language_color, language_name) {
+            (Some(color), Some(name)) => Some(Language::new(color, name)),
+            _ => None
         };
 
         repos.push(PinnedRepo::new(
             name,
             description,
             stars,
-            forks
+            forks,
+            language
         ));
     }
 
